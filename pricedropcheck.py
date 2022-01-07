f57c0fa8d81dc4cdab22edbe1a578b59af595175
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-07 09:11:22 +0530

    response validation

--- pricedropcheck.py
+++ pricedropcheck.py
@@ -5,11 +5,21 @@ import os
 import urllib.request
 
 # https://www.amazon.in/Xbox-Wireless-Controller-Robot-White/dp/B08K3GW17S
-url = input("Enter the URL of the product: ")
-sauce = urllib.request.urlopen(url).read()
-soup = bs4.BeautifulSoup(sauce, "html.parser")
-try:
-    prices = float(soup.find(class_="a-offscreen").get_text().replace("₹", "").replace(",", ""))
-except AttributeError():
-    prices = float(soup.find(class_="a-price-whole").get_text().replace(",", "").replace(".", ""))
-print(prices)
+def get_url(url):
+    url = input("Enter the URL of the product: ")
+    response = str(requests.get(url)).split(" ")
+    while response[1] != "[200]>":
+        print("Invalid URL. Try Again.")
+        url = input("Enter the URL of the product: ")
+        response = str(requests.get(url)).split(" ")
+    return url
+
+def price(url):
+    sauce = urllib.request.urlopen(url).read()
+    soup = bs4.BeautifulSoup(sauce, "html.parser")
+    try:
+        prices = float(soup.find(class_="a-offscreen").get_text().replace("₹", "").replace(",", ""))
+    except AttributeError():
+        prices = float(soup.find(class_="a-price-whole").get_text().replace(",", "").replace(".", ""))
+    print(prices)
+    return price
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+https://www.amazon.in/Xbox-Wireless-Controller-Robot-White/dp/B08K3GW17S
