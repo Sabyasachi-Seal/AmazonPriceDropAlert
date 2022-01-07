809088f8a95731902db92ac8615b6b00558577b0
Sabyasachi_Seal <36451386+Sabyasachi-Seal@users.noreply.github.com>
2022-01-07 09:36:09 +0530

    created temp price file

--- pricedropcheck.py
+++ pricedropcheck.py
@@ -3,6 +3,8 @@ import requests
 import tkinter
 import os
 import urllib.request
+import csv
+from datetime import datetime
 
 # https://www.amazon.in/Xbox-Wireless-Controller-Robot-White/dp/B08K3GW17S
 def get_url(url):
@@ -14,6 +16,16 @@ def get_url(url):
         response = str(requests.get(url)).split(" ")
     return url
 
+def save_price(price_list):
+    field = ['Time', 'Price']
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    data = [[dt_string, str(price_list)]]
+    with open("prices.csv", 'w') as price_file:
+        writer = csv.writer(price_file)
+        writer.writerow(field)
+        writer.writerows(data)
+
 def price(url):
     sauce = urllib.request.urlopen(url).read()
     soup = bs4.BeautifulSoup(sauce, "html.parser")
@@ -21,5 +33,9 @@ def price(url):
         prices = float(soup.find(class_="a-offscreen").get_text().replace("₹", "").replace(",", ""))
     except AttributeError():
         prices = float(soup.find(class_="a-price-whole").get_text().replace(",", "").replace(".", ""))
-    print(prices)
-    return price
+    cpmpare(price)
+    save_price(price)
+
+def compare(price):
+    with open("prices.csv", "r") as price_file:
+        print()
